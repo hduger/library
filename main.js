3d684fdba9b1665e8c9b617cb1380a25aa4df17c
Hunter Duger <hduger1@gmail.com>
2022-08-01 15:12:21 -0400

    Styled Form that will popup on click

---
+++ main.js
@@ -0,0 +1,24 @@
+
+let myLibrary = [];
+
+function Book(title, author, numPages, read) {
+    this.title = title;
+    this.author = author;
+    this.numPages = numPages;
+    this.read = read;
+    this.info = function() {
+        if (read===true){
+            return (`${title} by ${author}, ${numPages} pages, has been read`);
+        }else {
+            return (`${title} by ${author}, ${numPages} pages, has not been read`);
+        }
+    }
+}
+
+// const theHobbit = new Book('The Hobbit', "J.R.R. Tolkien", 295, true)
+
+// console.log(theHobbit.info());
+
+function addBookToLibrary() {
+    
+}
